4344edc2f6097680baf4a02fec34f2632e9f1fd8
sumzum <113144777+sumzum@users.noreply.github.com>
2022-09-09 00:32:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import os, time, asyncio, json, discord
+from discord.ext import commands
 
+#prefix-settings
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+#online-statement
+@bot.event
+async def on ready():
+    print("Bot is now Online")
